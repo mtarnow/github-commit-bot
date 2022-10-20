347a769f0650c455a4cae12fb26ce927e189b7f8
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-10-20 23:53:14 +0200

    Very important change No. 18

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ import subprocess
 
 commit_string = "I like apples. I like pears and the next day I committed and the next day I committed and committed " \
                 "again and committed again and the next day I committed and the next day I committed and the next " \
-                "day I didn't commit. Today I committed."
-n_commits = 17
+                "day I didn't commit. Today I committed and committed after that."
+n_commits = 18
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
