ddf0a488f6a5dd86da6f7ad41483e2d3d8352ffe
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-10-20 22:58:48 +0200

    "Very important change No. 0"

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import random
 import subprocess
 
 commit_string = "19.10.2022 was the first day. On that day I laid in bed and the next day I didn't commit. Today I " \
-                "committed."
-n_commits = 1
+                "committed and then committed."
+n_commits = 2
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
