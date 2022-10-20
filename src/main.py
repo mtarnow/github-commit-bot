69f6b8acb2c73569f085662c432e2d98ad3b74fa
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-10-20 23:19:44 +0200

    Very important change No. 7

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import subprocess
 
 commit_string = "19.10.2022 was the first day. On that day I laid in bed and the next day I committed and the next " \
                 "day I committed and committed after that and committed after that and then committed. Today I " \
-                "committed."
-n_commits = 6
+                "committed and committed."
+n_commits = 7
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
