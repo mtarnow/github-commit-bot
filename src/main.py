0086d2e4e86396451f5278dfe19b0605278f2aa9
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-10-20 23:29:02 +0200

    Very important change No. 11

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ import random
 import subprocess
 
 commit_string = "19.10.2022 was the first day. On that day I laid in bed and the next day I committed and committed " \
-                "and then committed and committed after that and committed again. Today I committed and committed " \
-                "after that and committed again and committed and committed."
-n_commits = 10
+                "and then committed and committed after that and committed again and the next day I committed and " \
+                "committed after that and committed after that and committed again and committed after that. Today I " \
+                "committed."
+n_commits = 11
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
