8ada080c99ab7dc425d7d3bb1b0dd86348f8d7b8
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-10-25 20:00:02 +0200

    Make a very important change No. 3

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import random
 import subprocess
 
 commit_string = "I started to work here on 25.10.2022. On that day I had to clean up the development mess. Today I " \
-                "committed and then committed."
-n_commits = 2
+                "committed and then committed and then committed."
+n_commits = 3
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
