9a6f53fa810fe483278c19fd8b22315cf6c2e04e
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-11-01 20:00:02 +0100

    Make a very important change No. 4

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import subprocess
 
 commit_string = "I started to work here on 26.10.2022. On that day I had to clean up the development mess and the " \
                 "next day I committed and then committed and the next day I didn't commit and the next day I didn't " \
-                "commit and the next day I didn't commit. Today I committed."
-n_commits = 3
+                "commit and the next day I didn't commit. Today I committed and then committed."
+n_commits = 4
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
