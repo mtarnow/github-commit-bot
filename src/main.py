61d2d23b33c70550b487d3996c9e4e4fa2f7da63
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2022-11-06 20:00:02 +0100

    Make a very important change No. 20

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ commit_string = "I started to work here on 26.10.2022. On that day I had to clea
                 "commit and the next day I didn't commit and the next day I committed and then committed and then " \
                 "committed and then committed and the next day I committed and committed after that and then " \
                 "committed and committed again and then committed and the next day I committed and committed after " \
-                "that and committed and then committed. Today I committed and then committed and then committed and " \
-                "committed after that."
-n_commits = 19
+                "that and committed and then committed and the next day I committed and committed after that and " \
+                "committed after that and committed after that. Today I committed."
+n_commits = 20
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
