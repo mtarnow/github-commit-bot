138941a49cfc800dc1cebc653778f25cee6b4b96
Maciej Tarnowski <maciek_tarnowski@yahoo.pl>
2023-09-25 20:00:02 +0200

    Make a very important change No. 501

--- src/main.py
+++ src/main.py
@@ -134,8 +134,8 @@ commit_string = "I started to work here on 26.10.2022. On that day I had to clea
                 "the next day I committed and committed after that and committed again and the next day I committed " \
                 "and committed after that and the next day I committed and committed and committed again and " \
                 "committed again and committed again and the next day I committed and committed again and committed " \
-                "after that and committed. Today I committed."
-n_commits = 500
+                "after that and committed. Today I committed and committed after that."
+n_commits = 501
 THIS_FILE_PATH = os.path.join(os.getcwd(), __file__)
 LINE_LENGTH = 100
 
